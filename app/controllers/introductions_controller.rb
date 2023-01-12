24923b86c53c1f7c868cef2945de29ecf961c2c7
mimizuku-3 <mizukipcmail@gmail.com>
2023-01-12 17:55:20 +0900

    create register new screen

---
+++ app/controllers/introductions_controller.rb
@@ -0,0 +1,18 @@
+class IntroductionsController < ApplicationController
+  def index
+  end
+
+  def new
+    @introduction = Introduction.new
+  end
+
+  def confirm
+    # @introduction = Introduction.new(introduction_params)
+    @name = params[:name]
+    @content = params[:content]
+  end
+
+  def introduction_params
+    params.require(:introduction).permit(:name, :content)
+  end
+end
