68c5d79d26a223837d904b7fdae9b12aeccc594a
mimizuku-3 <mizukipcmail@gmail.com>
2023-01-26 19:07:12 +0900

    Add not null to tables

--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -1,4 +1,6 @@
 class Hobby < ApplicationRecord
   has_many :hobby_introductions
   has_many :introductions, through: :hobby_introductions, dependent: :destroy
+
+  validates :hobby_name, presence: true
 end
--- app/models/hobby_introduction.rb
+++ app/models/hobby_introduction.rb
@@ -1,4 +1,7 @@
 class HobbyIntroduction < ApplicationRecord
   belongs_to :introduction, touch: true
   belongs_to :hobby
+
+  validates :introduction_id, presence: true
+  validates :hobby_id, presence: true
 end
