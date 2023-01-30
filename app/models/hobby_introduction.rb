93ae950f9fa3c2bb7da006d867de4d442de550dc
mimizuku-3 <mizukipcmail@gmail.com>
2023-01-30 16:46:06 +0900

    remove validates from introduction_id and hobby_id

--- app/models/hobby_introduction.rb
+++ app/models/hobby_introduction.rb
@@ -1,7 +1,4 @@
 class HobbyIntroduction < ApplicationRecord
   belongs_to :introduction, touch: true
   belongs_to :hobby
-
-  validates :introduction_id, presence: true
-  validates :hobby_id, presence: true
 end
