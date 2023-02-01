aa8cfc92c72752695e0f6cf76450b97bb5a19200
mimizuku-3 <mizukipcmail@gmail.com>
2023-02-01 10:34:45 +0900

    Set Timezone Asia/Tokyo

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module SelfIntroduction
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Asia/Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
     #
